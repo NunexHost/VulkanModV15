6e367974f11c12f6f6a628b29bdb0b7d1f3e3e53
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2024-03-17 11:29:48 -0300

    Update DrawUtil.java

--- src/main/java/net/vulkanmod/render/util/DrawUtil.java
+++ src/main/java/net/vulkanmod/render/util/DrawUtil.java
@@ -10,49 +10,41 @@ import org.joml.Matrix4f;
 
 public class DrawUtil {
 
+    private static final BufferBuilder builder = RenderSystem.renderThreadTesselator().getBuilder();
+    private static final Matrix4f orthoMatrix = new Matrix4f().setOrtho(0.0F, 1.0F, 0.0F, 1.0F, 0.0F, 1.0F, true);
+
     public static void blitQuad() {
         blitQuad(0.0, 1.0, 1.0, 0.0);
     }
 
-    public static void drawTexQuad(BufferBuilder builder, double x0, double y0, double x1, double y1, double z,
+    public static void drawTexQuad(double x0, double y0, double x1, double y1, double z,
                                    float u0, float v0, float u1, float v1) {
-        Tesselator tesselator = RenderSystem.renderThreadTesselator();
-        BufferBuilder bufferbuilder = tesselator.getBuilder();
-        bufferbuilder.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_TEX);
-        bufferbuilder.vertex(x0, y0, z).uv(0.0F, 1.0F).endVertex();
-        bufferbuilder.vertex(x1, y0, z).uv(1.0F, 1.0F).endVertex();
-        bufferbuilder.vertex(x1, y1, z).uv(1.0F, 0.0F).endVertex();
-        bufferbuilder.vertex(x0, y1, z).uv(0.0F, 0.0F).endVertex();
+        builder.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_TEX);
+        builder.vertex(x0, y0, z).uv(u0, v0).endVertex();
+        builder.vertex(x1, y0, z).uv(u1, v0).endVertex();
+        builder.vertex(x1, y1, z).uv(u1, v1).endVertex();
+        builder.vertex(x0, y1, z).uv(u0, v1).endVertex();
 
-        BufferBuilder.RenderedBuffer renderedBuffer = bufferbuilder.end();
-
-        Renderer.getDrawer().draw(renderedBuffer.vertexBuffer(), VertexFormat.Mode.QUADS, renderedBuffer.drawState().format(), renderedBuffer.drawState().vertexCount());
-
+        Renderer.getDrawer().draw(builder.end().vertexBuffer(), VertexFormat.Mode.QUADS);
     }
 
     public static void blitQuad(double x0, double y0, double x1, double y1) {
-        Tesselator tesselator = RenderSystem.renderThreadTesselator();
-        BufferBuilder bufferbuilder = tesselator.getBuilder();
-        bufferbuilder.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_TEX);
-        bufferbuilder.vertex(x0, y0, 0.0D).uv(0.0F, 1.0F).endVertex();
-        bufferbuilder.vertex(x1, y0, 0.0D).uv(1.0F, 1.0F).endVertex();
-        bufferbuilder.vertex(x1, y1, 0.0D).uv(1.0F, 0.0F).endVertex();
-        bufferbuilder.vertex(x0, y1, 0.0D).uv(0.0F, 0.0F).endVertex();
+        builder.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_TEX);
+        builder.vertex(x0, y0, 0.0D).uv(0.0F, 1.0F).endVertex();
+        builder.vertex(x1, y0, 0.0D).uv(1.0F, 1.0F).endVertex();
+        builder.vertex(x1, y1, 0.0D).uv(1.0F, 0.0F).endVertex();
+        builder.vertex(x0, y1, 0.0D).uv(0.0F, 0.0F).endVertex();
 
-        BufferBuilder.RenderedBuffer renderedBuffer = bufferbuilder.end();
-
-        Renderer.getDrawer().draw(renderedBuffer.vertexBuffer(), VertexFormat.Mode.QUADS, renderedBuffer.drawState().format(), renderedBuffer.drawState().vertexCount());
-
+        Renderer.getDrawer().draw(builder.end().vertexBuffer(), VertexFormat.Mode.QUADS);
     }
 
     public static void drawFramebuffer(GraphicsPipeline pipeline, VulkanImage attachment) {
 
-       boolean shouldUpdate = Renderer.getInstance().bindGraphicsPipeline(pipeline);
+        boolean shouldUpdate = Renderer.getInstance().bindGraphicsPipeline(pipeline);
 
         VTextureSelector.bindTexture(attachment);
 
-        Matrix4f matrix4f = new Matrix4f().setOrtho(0.0F, 1.0F, 0.0F, 1.0F, 0.0F, 1.0F, true);
-        RenderSystem.setProjectionMatrix(matrix4f, VertexSorting.DISTANCE_TO_ORIGIN);
+        RenderSystem.setProjectionMatrix(orthoMatrix, VertexSorting.DISTANCE_TO_ORIGIN);
         PoseStack posestack = RenderSystem.getModelViewStack();
         posestack.pushPose();
         posestack.setIdentity();
